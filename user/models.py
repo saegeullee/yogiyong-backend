20d0af531f48050d88f41e676300e2bdebf62e67
devzunky <devzunky@gmail.com>
2019-10-29 00:49:34 +0900

    socialplatform model add

--- user/models.py
+++ user/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+
+class SocialPlatforms(models.Model):
+	platform_name=models.CharField(max_length=20)
  
 class Users(models.Model):
 	email=models.CharField(max_length=200, unique=True)
 	password=models.CharField(max_length=500)
 	nickname=models.CharField(max_length=400, null=True)
+	joined_platform=models.ForeignKey(SocialPlatforms, on_delete=models.SET_NULL, null=True)
 	class Meta:
 		db_table='users_list'
