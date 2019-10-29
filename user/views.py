d0743760c58c19efd09aced235a5787835ce91d5
devzunky <devzunky@gmail.com>
2019-10-29 20:23:23 +0900

    view에서 Error 처리, url에서 include 삭제

--- user/views.py
+++ user/views.py
@@ -8,46 +8,53 @@ from .models	  import Users
 class SignUpView(View):
 	def post(self, request):
 		input_data=json.loads(request.body)
-		#존재하는 유저인지 체크
-		if Users.objects.filter(email=input_data['email']).exists():	
-			#이미 존재하는 유저이므로 409 conflict		
-			return JsonResponse({'message':'USER_EXISTS'}, status=409)
-		else:
-			#패스워드 암호화
-			encoded_password=input_data['password'].encode('utf-8')
-			hashed_password=bcrypt.hashpw(encoded_password, bcrypt.gensalt())
-			#DB에 저장
-			Users(
-				email=input_data['email'],
-				password=hashed_password.decode('utf-8'), #CharField이기 때문에 unicode로 넣어줌
-				nickname=input_data['nickname'],
-				email_accept=input_data['email_accept'],
-				phone_number=input_data['phone_number'],
-				).save()
-			
-			return JsonResponse({'message':'SUCCESS'}, status=200)
-
+		try:
+			#존재하는 유저인지 체크
+			if Users.objects.filter(email=input_data['email']).exists():	
+				#이미 존재하는 유저이므로 409 conflict		
+				return JsonResponse({'message':'USER_EXISTS'}, status=409)
+			else:
+				#패스워드 암호화
+				encoded_password=input_data['password'].encode('utf-8')
+				hashed_password=bcrypt.hashpw(encoded_password, bcrypt.gensalt())
+				#DB에 저장
+				Users(
+					email=input_data['email'],
+					password=hashed_password.decode('utf-8'), #CharField이기 때문에 unicode로 넣어줌
+					nickname=input_data['nickname'],
+					email_accept=input_data['email_accept'],
+					phone_number=input_data['phone_number'],
+					).save()
+				
+				return JsonResponse({'message':'SUCCESS'}, status=200)
+		except KeyError:
+			return JsonResponse({'message':'WRONG_KEY'}, status=400)
 class SignInView(View):
 	def get(self, request):
 		input_data=json.loads(request.body)
-		#기존 회원인지 확인
-		if Users.objects.filter(email=input_data['email']).exists():
-			user_in_db=Users.objects.get(email=input_data['email'])
-			#패스워드 일치 확인
-			if bcrypt.checkpw(input_data['password'].encode('utf-8'), user_in_db.password.encode('utf-8')):
-				#토큰 발행
-				token=jwt.encode({'id':user_in_db.id}, 'secret', algorithm='HS256')
+		try:	
+			#기존 회원인지 확인
+			if Users.objects.filter(email=input_data['email']).exists():
+				user_in_db=Users.objects.get(email=input_data['email'])
+				#패스워드 일치 확인
+				if bcrypt.checkpw(input_data['password'].encode('utf-8'), user_in_db.password.encode('utf-8')):
+					#토큰 발행
+					token=jwt.encode({'id':user_in_db.id}, 'secret', algorithm='HS256')
 			
-				return JsonResponse(
-							{
-							'message':f'Hi, {user_in_db.nickname}',
-							'token':  f'{token}'
-							},status=200
-						)
-			#패스워드가 일치하지 않음
-			else:
+					return JsonResponse(
+								{
+								'message':f'Hi, {user_in_db.nickname}',
+								'token':  f'{token}'
+								},status=200
+							)
+				#패스워드가 일치하지 않음
+				else:
 				
-				return JsonResponse({'message':'INVALID_PASSWORD'}, status=409)
-		else:
-			#없는 유저이므로 401 Unauthorized
+					return JsonResponse({'message':'INVALID_PASSWORD'}, status=409)
+			else:
+				#없는 유저이므로 401 Unauthorized
+				return JsonResponse({'message':'INVALID_USER'}, status=401)
+		except KeyError:
+			return JsonResponse({'message':'WRONG_KEY'}, status=400)
+		except Users.DoesNotExist:
 			return JsonResponse({'message':'INVALID_USER'}, status=401)
