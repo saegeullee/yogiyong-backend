94860c2e03278c9cd64f4bae8a59fdee412ba341
devzunky <devzunky@gmail.com>
2019-10-29 17:25:48 +0900

    sign up, sign in modified

--- user/models.py
+++ user/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 class SocialPlatforms(models.Model):
 	platform_name=models.CharField(max_length=20)
- 
+	user=models.ManyToManyField('Users')
+	
+	class Meta:
+		db_table='SocialPlatforms'
+
 class Users(models.Model):
-	email=models.CharField(max_length=200, unique=True)
-	password=models.CharField(max_length=500)
-	nickname=models.CharField(max_length=400, null=True)
-	joined_platform=models.ForeignKey(SocialPlatforms, on_delete=models.SET_NULL, null=True)
+	email=models.CharField(max_length=200, unique=True, null=False)
+	password=models.CharField(max_length=400)
+	email_accept=models.BooleanField(default=False)
+	nickname=models.CharField(max_length=50, blank=True)
+	phone_number=models.CharField(max_length=100, blank=True)
+	
 	class Meta:
 		db_table='users_list'
