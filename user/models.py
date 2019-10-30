fdaa4f19e729dfa89b8a84be3379dcafbc2afb5e
devzunky <devzunky@gmail.com>
2019-10-30 17:16:52 +0900

    model.py -> user, social platform relationship modified

--- user/models.py
+++ user/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
-class SocialPlatforms(models.Model):
-    platform_name=models.CharField(max_length=20)
-    user=models.ManyToManyField('Users')
+class SocialPlatform(models.Model):
+    name=models.CharField(max_length=20)
 	
     class Meta:
-        db_table='SocialPlatforms'
+        db_table='social_platforms'
 
-class Users(models.Model):
-    email=models.CharField(max_length=70, unique=True, null=False)
+class User(models.Model):
+    email=models.CharField(max_length=70, unique=True)
     password=models.CharField(max_length=400)
     email_accept=models.BooleanField(default=False)
     nickname=models.CharField(max_length=50, blank=True)
-	
+    social_platform=models.ForeignKey(SocialPlatform, on_delete=models.SET_NULL, null=True)
+    
     class Meta:
-        db_table='users_list'
+        db_table='users'
